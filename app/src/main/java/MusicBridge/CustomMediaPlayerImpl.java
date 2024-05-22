dc60150aa7515ec9daca6954fab0c99c8ae4ddb3
Azmain's - Portfolio <azmainm2017@gmail.com>
2024-05-22 22:32:46 +0600

    Applied bridge pattern on music

---
+++ app/src/main/java/MusicBridge/CustomMediaPlayerImpl.java
@@ -0,0 +1,18 @@
+package MusicBridge;
+
+// CustomMediaPlayerImpl.java
+
+
+public class CustomMediaPlayerImpl implements MediaPlayerImpl {
+    // Implement custom media player functionality here
+    @Override
+    public junit.framework.TestResult start() {
+        // Custom implementation for starting media playback
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        // Custom implementation for stopping media playback
+    }
+}
---
+++ app/src/main/java/MusicBridge/MediaPlayerImpl.java
@@ -0,0 +1,6 @@
+package MusicBridge;
+
+public interface MediaPlayerImpl {
+    junit.framework.TestResult start();
+  void stop();
+}
