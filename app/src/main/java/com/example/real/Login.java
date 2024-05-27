3d00c705cee74f7cb634243822d44b3f2cf04ddf
SayakaMeem <sayakaalam419@gmail.com>
2024-05-27 10:35:59 +0600

    register

--- app/src/main/java/com/example/real/Login.java
+++ app/src/main/java/com/example/real/Login.java
@@ -2,7 +2,11 @@ package com.example.real;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        final Button loginBtn=findViewById(R.id.loginBtn);
+        final TextView registerNowBtn=findViewById(R.id.registerNowBtn);
+        registerNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+
+});
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, MainActivity2.class));
+
+            }
+        });
     }
 }
